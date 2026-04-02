879a253812ff8acd57aa902aaef36c96c9998d6f
cembykz <121633027+cembykz@users.noreply.github.com>
2026-04-02 16:31:26 +0300

    Add Main, ISport, Football, GameStatus

---
+++ src/Football.java
@@ -0,0 +1,19 @@
+public class Football {
+    private int maxPlayersPerTeam;
+    private int playersOnField;
+    private int maxSubstitutions;
+    private int matchDuration;
+
+    public int getMaxPlayersPerTeam(){
+        return maxPlayersPerTeam;
+    };
+    public int getPlayersOnField(){
+        return playersOnField;
+    };
+    public int getMaxSubstitutions(){
+        return maxSubstitutions;
+    };
+    public int getMatchDuration(){
+        return matchDuration;
+    };
+}
---
+++ src/GameStatus.java
@@ -0,0 +1,8 @@
+public class GameStatus {
+    public static final String NOT_STARTED = "NOT_STARTED";
+    public static final String IN_PROGRESS = "IN_PROGRESS";
+    public static final String FINISHED = "FINISHED";
+
+
+
+}
