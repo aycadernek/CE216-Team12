9df0d944254100ac8e5bbd55f69d4bc6ffc9360b
cembykz <121633027+cembykz@users.noreply.github.com>
2026-04-04 14:21:50 +0300

    Updated ISport.java

--- src/ISport.java
+++ src/ISport.java
@@ -1,3 +1,40 @@
-public class ISport {
+import java.util.List;
+
+public interface ISport {
+
+    //Getters 
+    String getSportName();
+    int getPlayerCount();
+    int getSubstituteCount();
+    int getSubstituteChangeLimit();
+    int getTeamCount();
+    int getPeriodCount();
+    List<String> getAvailableTactics();
+    int getMatchupsPerTeam();
     
+    /**
+    * Provides the points system for the sport.
+    * Used by other components to assign points dynamically
+    * instead of hardcoding values (e.g., 3/1/0 in football).
+    */
+    int getWinPoint();
+    int getDrawPoint();
+    int getLossPoint();
+
+    //Setters
+    void setSportName(String sportName);
+    void setPlayerCount(int playerCount);
+    void setSubstituteCount(int substituteCount);
+    void setSubstituteChangeLimit(int substituteChangeLimit);
+    void setTeamCount(int teamCount);
+    void setWinPoint(int winPoint);
+    void setDrawPoint(int drawPoint);
+    void setLossPoint(int lossPoint);
+    void setPeriodCount(int periodCount);
+    void setAvailableTactics(List<String> availableTactics);
+    void setMatchupsPerTeam(int matchupsPerTeam);
+
+    // Core Methods
+    void generateLeague();
+    void generateTeams();
 }
